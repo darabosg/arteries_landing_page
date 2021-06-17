462a2bd478876c86744cad9f4deb9ff9c8d2232d
darabosg <sconty@gmail.com>
2021-06-17 12:40:20 +0200

    circle style

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -2,10 +2,7 @@ import React from 'react'
 import './Circle.scss'
 
 const Circle = () => {
-    return (
-        <div className="circle">
-        </div>
-    )
+    return <div className='circle'></div>
 }
 
 export default Circle
